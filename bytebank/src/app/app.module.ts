d8b1b3d6b51af647526bd0efb77f2feb2f513756
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-07-07 17:59:11 -0300

    Byte Bank

---
+++ bytebank/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+
+@NgModule
+({
+  declarations:
+  [
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
+  ],
+  imports:
+  [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers:
+  [
+
+  ],
+  bootstrap:
+  [
+    AppComponent
+  ]
+})
+export class AppModule { }
